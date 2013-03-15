afd97faf529eaf0faa0eb0177bc4241e5789c798
John <chiodini@bu.edu>
2013-03-14 20:42:51 -0400

    Fixed Issues With Textures, made Directory more Robust

--- Start_Screen_Graphical/test_form.Designer.cs
+++ Start_Screen_Graphical/test_form.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,20 +42,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "purple";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // test_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "test_form";
-            this.Text = "test_form";
+            this.Text = "purple";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
